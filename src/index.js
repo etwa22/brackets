1adf999ff9a5a73d305d429b542e8ee4dfef9802
github-username <etwa@ngs.ru>
2023-08-20 14:56:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (str.length > 0) {
+    var output = str
+    .replace("{}", "")
+    .replace("[]", "")
+    .replace("()", "")
+    .replace("||", "")
+    .replace("11", "")
+    .replace("22", "")
+    .replace("33", "")
+    .replace("44", "")
+    .replace("55", "")
+    .replace("66", "")
+    .replace("77", "")
+    .replace("88", "")
+    .replace("12", "")
+    .replace("34", "")
+    .replace("56", "");
+        if (str == output) return false;
+        str = output;
+    }
+    return true;
 }
